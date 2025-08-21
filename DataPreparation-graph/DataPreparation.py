db10b284300f09d0ef078729ccdc098ddb378d55
Alder998 <davide.ontano@yahoo.com>
2025-08-21 09:28:40 +0200

    Commit G-2.2: implemented creation of 4D tensored feature Matrix with windows + train-validation-test split (everything is ready to be used by the Graph Model)

--- DataPreparation-graph/DataPreparation.py
+++ DataPreparation-graph/DataPreparation.py
@@ -14,6 +14,7 @@ from DatabaseManager import Database as db
 from DatabaseManager import DatabasePlugin_dask as dk
 from geopy.distance import geodesic
 from ModelStorageService import ModelStorageService as st
+from sklearn.model_selection import GroupShuffleSplit
 
 class DataPreparation:
 
@@ -83,8 +84,44 @@ class DataPreparation:
 
         return tcheck
 
+    # Utils-like function to have a time-spacial split for the data
+    def timeSpaceSplit (self, dataInDataFrameFormat, test_size=0.3, validation_size=0.15):
+
+        # 1. Apply space split with nodes
+        # 1.1. Create node_id
+        dataInDataFrameFormat['node_id'] = dataInDataFrameFormat.groupby(['latitude', 'longitude']).ngroup()
+        # 1.2. Split using sk-learn
+        splitter = GroupShuffleSplit(test_size=test_size, n_splits=1, random_state=1893)
+        train_idx, test_idx = next(splitter.split(dataInDataFrameFormat, groups=dataInDataFrameFormat['node_id']))
+        # 1.3. Divide the DataFrame
+        df_train_spatial = dataInDataFrameFormat.iloc[train_idx]
+        df_test_spatial = dataInDataFrameFormat.iloc[test_idx]
+
+        # 2. Time split
+        # 2.1. Sort by date
+        df_train_spatial = df_train_spatial.sort_values('date')
+        df_test_spatial = df_test_spatial.sort_values('date')
+
+        # 2.2. Get the unique time stamps
+        times = sorted(df_train_spatial['date'].unique())
+        # 2.3 Create the thresholds for time
+        train_end = int(len(times) * (1-test_size))
+        val_end = int(len(times) * (1-test_size+validation_size))
+
+        # 2.4. divide the DataFrame index
+        train_time = times[:train_end]
+        val_time = times[train_end:val_end]
+        test_time = times[val_end:]
+
+        # 2.5. Apply the time split
+        train_final = df_train_spatial[df_train_spatial['date'].isin(train_time)]
+        val_final = df_train_spatial[df_train_spatial['date'].isin(val_time)]
+        test_final = df_test_spatial[df_test_spatial['date'].isin(test_time)]
+
+        return train_final, test_final, val_final
+
     # Utils-like function to process data from DataFrame to Graph
-    def processDataFromDataFrameToGraph (self, dataInDataFrameFormat, variableToPredict=[], distance_threshold=100):
+    def createAdjacencyMatrix (self, dataInDataFrameFormat, distance_threshold=100):
 
         # 1. Create the Adjacency matrix
         # 1.1. Compute node id as incremental index
@@ -108,11 +145,18 @@ class DataPreparation:
         D_inv = np.linalg.inv(D_hat)
         adj_matrix_norm = D_inv @ (adj_matrix + np.eye(adj_matrix.shape[0]))
 
+        return adj_matrix_norm
+
+    def createFeaturesMatrix (self, dataInDataFrameFormat, variableToPredict=[]):
+
+        # 1. get the number of unique coords
+        coords = len(dataInDataFrameFormat[['latitude', 'longitude']].drop_duplicates().values)
+
         # 2. Create the Feature Matrix: that is where you store your variables of interest
         for enumUniqueTS, uniqueTS in enumerate(dataInDataFrameFormat['date'].unique()):
             dataInDataFrameFormat.loc[dataInDataFrameFormat["date"] == uniqueTS, "time_index"] = enumUniqueTS
         # 2.1. Initialize the empty matrix for features: the shape must be (grid_steps, variables, time steps)
-        feature_matrix = np.zeros((adj_matrix_norm.shape[0], len(variableToPredict), len(dataInDataFrameFormat['date'].unique())))
+        feature_matrix = np.zeros((coords, len(variableToPredict), len(dataInDataFrameFormat['date'].unique())))
 
         # 2.2. Map indexes and nodes, and fill the matrix
         node_ids = {(lat, lon): i for i, (lat, lon) in enumerate(dataInDataFrameFormat[['latitude', 'longitude']].drop_duplicates().values)}
@@ -122,23 +166,68 @@ class DataPreparation:
             for f_idx, feature in enumerate(variableToPredict):
                 feature_matrix[i, f_idx, t] = row[feature]
 
-        return adj_matrix_norm, feature_matrix
+        return feature_matrix
+
+    # Function to create model-ready tensors
+    def createModelTensors (self, set, window_size, horizon):
+
+        # 1. Initialize the shapes as you have them
+        num_nodes, num_features, time_steps = set.shape
+        num_samples = time_steps - window_size - horizon + 1
+
+        # 2. Initialize the tensors to be used as set and targets
+        sample = np.zeros((num_samples, num_nodes, num_features, window_size))
+        target = np.zeros((num_samples, num_nodes, num_features, horizon))
+
+        # 3. create the rolling windows
+        for i in range(num_samples):
+            sample[i] = set[:, :, i: i + window_size]
+            target[i] = set[:, :, i + window_size: i + window_size + horizon]
+
+        return sample, target
 
     # Main function to prepare data for graphs processing
-    def prepareDataForGraphModel (self, start_date, end_date, variableToPredict):
+    def prepareDataForGraphModel (self, start_date, end_date, variableToPredict, test_size, validation_size,
+                                  window_size, horizon, distance_threshold):
 
         # 0. Get data from database
         print("DATA PREPARATION - Extracting data from Database...")
         dataInDataFrameFormat = self.getDataWindow(start_date=start_date, end_date=end_date)
 
-        # 1. Transform from dataFrame to Graph
+        # 1. time-space Split with appropriate libraries
+        train_set, test_set, validation_set = self.timeSpaceSplit(dataInDataFrameFormat=dataInDataFrameFormat,
+                                                                 test_size=test_size,
+                                                                 validation_size=validation_size)
+
+        # 2. Create Adjacency Matrix for each one of the sets
         print("DATA PREPARATION - Converting DataFrame into graph...")
-        adj_matrix_norm, feature_matrix = self.processDataFromDataFrameToGraph(dataInDataFrameFormat=dataInDataFrameFormat,
-                                                          distance_threshold=100,
-                                                          variableToPredict=variableToPredict)
-        print("DATA PREPARATION - INFO: Shape of normalized Adjacency Matrix: ", adj_matrix_norm.shape)
-        print("DATA PREPARATION - INFO: Shape of Feature Matrix: ", feature_matrix.shape)
+        adj_matrix_norm_train = self.createAdjacencyMatrix(dataInDataFrameFormat=train_set,
+                                                                distance_threshold=distance_threshold)
+        print("DATA PREPARATION - INFO (TRAIN SET): Shape of normalized Adjacency Matrix: ", adj_matrix_norm_train.shape)
+        adj_matrix_norm_test = self.createAdjacencyMatrix(dataInDataFrameFormat=test_set,
+                                                                distance_threshold=distance_threshold)
+        print("DATA PREPARATION - INFO (TEST SET): Shape of normalized Adjacency Matrix: ", adj_matrix_norm_test.shape)
+        adj_matrix_norm_validation = self.createAdjacencyMatrix(dataInDataFrameFormat=validation_set,
+                                                                distance_threshold=distance_threshold)
+        print("DATA PREPARATION - INFO (VALIDATION SET): Shape of normalized Adjacency Matrix: ", adj_matrix_norm_validation.shape)
 
-        # 2. time-space Split with appropriate libraries + obtain Model-ready params
+        # 3. Create feature Matrix for each one of the sets
+        feature_matrix_train = self.createFeaturesMatrix(dataInDataFrameFormat=train_set,
+                                                         variableToPredict=variableToPredict)
+        feature_matrix_test = self.createFeaturesMatrix(dataInDataFrameFormat=test_set,
+                                                        variableToPredict=variableToPredict)
+        feature_matrix_validation = self.createFeaturesMatrix(dataInDataFrameFormat=validation_set,
+                                                              variableToPredict=variableToPredict)
 
-        return adj_matrix_norm, feature_matrix
+        # 4. Create model-ready tensors
+        sample_train, target_train = self.createModelTensors(set=feature_matrix_train, window_size=window_size, horizon=horizon)
+        print("DATA PREPARATION - INFO (TRAIN SET): Shape of Sample Matrix: ", sample_train.shape)
+        print("DATA PREPARATION - INFO (TRAIN SET): Shape of Target Matrix: ", target_train.shape)
+        sample_test, target_test = self.createModelTensors(set=feature_matrix_test, window_size=window_size, horizon=horizon)
+        print("DATA PREPARATION - INFO (TEST SET): Shape of Sample Matrix: ", sample_test.shape)
+        print("DATA PREPARATION - INFO (TEST SET): Shape of Target Matrix: ", target_test.shape)
+        sample_validation, target_validation = self.createModelTensors(set=feature_matrix_validation, window_size=window_size, horizon=horizon)
+        print("DATA PREPARATION - INFO (VALIDATION SET): Shape of Sample Matrix: ", sample_validation.shape)
+        print("DATA PREPARATION - INFO (VALIDATION SET): Shape of Target Matrix: ", target_validation.shape)
+
+        return adj_matrix_norm_train, adj_matrix_norm_test, adj_matrix_norm_validation, sample_train, target_train, sample_test, target_test, sample_validation, target_validation
--- DataPreparation-graph/DataPreparation_test.py
+++ DataPreparation-graph/DataPreparation_test.py
@@ -1,10 +1,22 @@
 import DataPreparation as dt
 import numpy as np
+import time
+
+start = time.time()
 
 # Instantiate the class
 classModule = dt.DataPreparation(grid_step=0.22)
 
 # Train-test split
-adj_matrix_norm, feature_matrix = classModule.prepareDataForGraphModel(start_date = "2025-06-01",
-                                                                       end_date = "2025-06-20",
-                                                                       variableToPredict=["temperature"])
+adj_matrix_norm_train, adj_matrix_norm_test, adj_matrix_norm_validation, sample_train, target_train, sample_test, target_test, sample_validation, target_validation = classModule.prepareDataForGraphModel(
+                                                                       start_date = "2025-06-01",
+                                                                       end_date = "2025-08-15",
+                                                                       variableToPredict=["temperature"],
+                                                                       test_size=0.30,
+                                                                       validation_size=0.15,
+                                                                       window_size=24,
+                                                                       horizon=96,
+                                                                       distance_threshold=50)
+
+end = time.time()
+print("\nTime Elapsed for dataset preparation: " + str(round((end-start) / 60, 2)) + " minutes")
