f142d6390e02b6df9f3ad1841bc0128f6deae34c
Alder998 <davide.ontano@yahoo.com>
2025-10-27 00:01:32 +0100

    Commit G-3.14: created the structure to gather all the modelService variables and params in one single wrapper (ModelServiceWrapper), to be best used for batch execution and model parameter tuning.

---
+++ ModelService_graph/ModelServiceWrapper.py
@@ -0,0 +1,14 @@
+"""
+This class will be maily used to organize better model inputs for the launch's sake
+"""
+
+from DataPreparation_graph import DataPreparation as dt
+import ModelService as model
+
+class ModelServiceWrapper:
+    def __init__(self):
+        pass
+
+    def trainAndSaveGraphModel (self):
+
+        return 0
--- ModelService_graph/PredictionMaker.py
+++ ModelService_graph/PredictionMaker.py
@@ -4,14 +4,14 @@ import PredictionService as p
 from ReportingLibrary import Animations as ani
 from ReportingLibrary import LocalizedWeather as locl
 
-model_name = "graph-3mo-1v-96h"
+model_name = "graph-3mo-2v-96h"
 
 # Instantiate the class
 prediction_data = p.PredictionService(model=model_name).predictWithStoredModel(grid_step=0.22, start_date="2025-10-07")
 
 # Report Part
 animation = ani.Animations().generateAnimationOnWeatherVariableFromDataFrame(dataFrame=prediction_data,
-                                                                             weatherVariable="temperature",
+                                                                             weatherVariable="windSpeed",
                                                                              start_date=None,
                                                                              end_date=None,
                                                                              colorScale="rainbow",
@@ -19,6 +19,6 @@ animation = ani.Animations().generateAnimationOnWeatherVariableFromDataFrame(dat
                                                                              show=True)
 
 timeSeriesForCity = locl.LocalizedWeather().getPredictionTimeSeriesOnTargetVariable (predictedDf=prediction_data,
-                                                                                     city='Milano',
-                                                                                     predictedVariable="temperature",
+                                                                                     city='Lavagna',
+                                                                                     predictedVariable="windSpeed",
                                                                                      confidence_levels=False)
